1c20f50671c795c8c8f48121582548abcdefc217
Edney Fillipi <61793102+efillipi@users.noreply.github.com>
2021-07-12 10:44:50 -0300

    :hammer: ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'default',
+    type: process.env.DB_CLIENT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    entities: [process.env.TYPEORM_ENTITIES],
+    migrations: [process.env.TYPEORM_MIGRATION],
+    cli: {
+      migrationsDir: process.env.TYPEORM_CLI,
+    },
+  },
+
+  {
+    name: 'mongo',
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    database: 'gobarber',
+    useUnifiedTopology: true,
+    entities: ['./src/modules/**/infra/typeorm/schemas/*.ts'],
+  },
+];
